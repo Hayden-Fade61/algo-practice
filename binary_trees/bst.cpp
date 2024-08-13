c3235ac42fc89d5ced488abbcdcd3c9f9742947e
Sai <63241537+Hayden-Fade61@users.noreply.github.com>
2024-08-13 09:02:33 +0200

    started work on node removal

--- binary_trees/bst.cpp
+++ binary_trees/bst.cpp
@@ -25,62 +25,91 @@ class bst{
             this -> root = nullptr;
         }
         bst (int data){
-            this -> root = new node(data);
+            this  ->  root = new node(data);
         }
-        node_t* getRoot() {return this->root;}
+        node_t* getRoot() {return this -> root;}
 
         // Insert / Delete 
         void insert(node_t* root, int newVal){
-            bool goLeft = root -> val <= newVal;
-            if (goLeft && root->left == nullptr) root->left = new node(newVal); 
-            else if (goLeft && root->left != nullptr) insert(root->left, newVal); 
-            else if (!goLeft && root->right == nullptr) root->right = new node(newVal); 
-            else insert(root->right, newVal); 
+            if (root -> val == newVal) return;
+            bool goLeft = root -> val < newVal;
+            if (goLeft && root -> left == nullptr) root -> left = new node(newVal); 
+            else if (goLeft && root -> left != nullptr) insert(root -> left, newVal); 
+            else if (!goLeft && root -> right == nullptr) root -> right = new node(newVal); 
+            else insert(root -> right, newVal); 
         }
 
         node_t* remove(node_t *root, int key){
-            
+            node_t* toDelete = root;
+            node_t* parent = nullptr;
+            // Find value if it exists in the tree
+            while(toDelete -> val != key && toDelete != nullptr){
+                if (toDelete -> val < key){
+                    parent = toDelete;
+                    toDelete = toDelete -> left;
+                }
+                else{
+                    parent = toDelete;
+                    toDelete = toDelete -> right;
+                }
+            }
+            if (toDelete == nullptr) return nullptr;
+            // 0 child, just delete the node
+            if(toDelete->left == nullptr && toDelete->right == nullptr){
+                return toDelete;
+            }
+            // 1 child, replace toDelete with child
+            else if(toDelete->left == nullptr || toDelete->right == nullptr){
+                node_t *child = toDelete->left == nullptr ? toDelete->left : toDelete->right;
+                if(parent -> left == toDelete) parent->left = child;
+                else parent->right = child;
+                return toDelete;
+            }
+            // 2 children
+            else{
+                
+            }
         }
 
         // Find methods
         node_t* find(node_t* root, int key){
             if(root == nullptr) return nullptr;
             if(root -> val == key) return root;
-            else if(key > root -> val) return find(root->right, key);
-            else return find(root->left, key);          
+            else if(key > root -> val) return find(root -> right, key);
+            else return find(root -> left, key);
         }
 
         node_t* findMin(node_t* root){
-            if(root->left == nullptr) return root;
-            findMin(root->left);
+            if(root -> left == nullptr) return root;
+            findMin(root -> left);
         }
 
         node_t* findMax(node_t* root){
-            if(root->right == nullptr) return root;
-            findMax(root->right);
+            if(root -> right == nullptr) return root;
+            findMax(root -> right);
         }
 
         // Traversal methods
         void inorderTraversal(node_t* root){
             if (root != nullptr){
-                inorderTraversal(root->left);
+                inorderTraversal(root -> left);
                 std::cout << root << std::endl;
-                inorderTraversal(root->right);
+                inorderTraversal(root -> right);
             }
         }
 
         void preorderTraversal(node_t* root){
             if (root != nullptr){
                 std::cout << root << std::endl;
-                inorderTraversal(root->left);
-                inorderTraversal(root->right);
+                inorderTraversal(root -> left);
+                inorderTraversal(root -> right);
             }
         }
 
         void postorderTraversal(node_t* root){
             if (root != nullptr){
-                    inorderTraversal(root->left);
-                    inorderTraversal(root->right);
+                    inorderTraversal(root -> left);
+                    inorderTraversal(root -> right);
                     std::cout << root << std::endl;
                 }
         }
